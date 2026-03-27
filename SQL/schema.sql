6ca419aaad675025611f9f5e6cce81b4af79fee2
Gaurav Sharma <gaurav.rsharma911@gmail.com>
2026-03-27 15:11:39 +0530

    Create schema.sql

---
+++ SQL/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE investments (
+    client_id VARCHAR,
+    age INT,
+    city VARCHAR,
+    asset_type VARCHAR,
+    risk_level VARCHAR,
+    provider VARCHAR,
+    total_investment NUMERIC,
+    current_value NUMERIC,
+    returns NUMERIC,
+    date DATE,
+    currency_type VARCHAR
+);
